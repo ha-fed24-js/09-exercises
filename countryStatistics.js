0b1ce518332ce05147f3e54274b421a1559b3025
David Andersson <lejonmanen@gmail.com>
2024-12-05 11:37:56 +0100

    3d

--- countryStatistics.js
+++ countryStatistics.js
@@ -8,6 +8,8 @@ async function countryClick() {
 	printCountryNames(data)
 	findFirstAfricanCountry(data)
 	womenInAustralia(data)
+	makePopulationList(data)
+	populationInEurope(data)
 }
 
 function printCountryNames(countries) {
@@ -45,9 +47,35 @@ function womenInAustralia(countries) {
 	}
 }
 
+function makePopulationList(countries) {
+	// Vi har: [ {continent, name, pFemale, population} ]
+	// Vi behöver: [ {name, men, women} ]
+	function makeObject(country) {
+		let women = Math.round(country.population * country.pFemale)
+		let men = country.population - women
+		return { name: country.name, men: men, women: women }
+	}
+	const newList = countries.map(makeObject)
+	console.log('Lista med könsfördelning:', newList)
+}
+
+function populationInEurope(countries) {
+	const europeCountries = countries.filter(country => country.continent === 'Europe')
+	// Två möjligheter: forEach + variabel eller reduce
+	let total = 0
+	europeCountries.forEach(country => {
+		total += country.population
+	})
+	printCountryData(`Det bor sammanlagt ${total} personer i Europa.`)
+}
+
 function printCountryData(message) {
 	const countryData = document.querySelector('#country-data')
-	countryData.innerText = message
+	const span = document.createElement('span')
+	const br = document.createElement('br')
+	span.innerText = message
+	countryData.append(span, br)
+
 	console.log(message)
 }
 
